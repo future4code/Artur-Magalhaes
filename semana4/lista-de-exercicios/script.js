c1b3b15f2809baa8c7c239d9aa4bcb5541478a67
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-05-08 11:14:53 -0300

    Exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,28 @@
+/*Interpretação de código
+1. A função pega uma entrada do usuário pelo prompt, converte para tipo number, calcula valor*cotação(entrada do usuário) e retorna concatenado com a string "R$".
+   
+Console exibirá: 
+R$(100*prompt)
+
+
+2. A função investeDinheiro recebe dois argumentos (tipoDeInvestimento, valor) e dependendo do tipoDeInvestimento passado o cálculo será realizado de uma forma diferente dos outros tipos e se o valor passado não for correspondente as opções será apresentado um alert.
+
+Console exibirá:
+165
+TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
+
+3. O código percorre o vetor numeros e armazena valores pares no array1 e ímpares no array2.
+
+Console exibirá:
+Quantidade total de números 14
+6
+8
+
+
+4. O código pega o maior e o menor número dentro do array.
+
+Console exibirá:
+-10
+283
+*/
