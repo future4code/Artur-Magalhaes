63858b6bb48fa024a7d3df061f5925b50ce7a8be
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-05-13 17:59:49 -0300

    Aula 19 - v2

--- semana5/insta4 2/src/components/Post/Post.js
+++ semana5/insta4 2/src/components/Post/Post.js
@@ -2,19 +2,25 @@ import React from 'react'
 import './Post.css'
 
 import {IconeComContador} from '../IconeComContador/IconeComContador'
-import {IconeMarcadorBranco} from '../../img/bookmark_branco.svg'
-import {IconeMarcadorPreto} from '../../img/bookmark-preto.svg'
+import iconeMarcadorBranco from '../../img/bookmark-branco.svg'
+import iconeMarcadorPreto from '../../img/bookmark-preto.svg'
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
+import iconeCompartilhar from '../../img/share.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoCompartilhar } from '../SecaoCompartilhar/SecaoCompartilhar'
 
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0
+    numeroComentarios: 0,
+    salvado: false,
+    numeroSalvados: 0,
+    compartilhar: false,
+    numeroCompartilhamento: 0
   }
 
   onClickCurtida = () => {
@@ -30,10 +36,37 @@ class Post extends React.Component {
       })
     }
   }
+  
+  onClickSalvar = () => {
+    if (this.state.salvado === false) {
+      this.setState({
+        salvado: !this.state.salvado,
+        numeroSalvados: this.state.numeroSalvados + 1
+      })
+    }
+    else{
+      this.setState({
+        salvado: !this.state.salvado,
+        numeroSalvados: this.state.salvado - 1
+      })
+    }
+  }
+
+  onClickCompartilhar = () => {
+    this.setState({
+      compartilhar: !this.state.compartilhar
+    })
+  }
 
   onClickComentario = () => {
     this.setState({
       comentando: !this.state.comentando
+    })
+  }
+  aoEnviarCompartilhar = () => {
+    this.setState({
+      compartilhar: false,
+      numeroCompartilhamento: this.state.numeroCompartilhamento + 1
     })
   }
 
@@ -51,6 +84,19 @@ class Post extends React.Component {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
+    }
+
+    let iconeSalvar
+    if(this.state.salvado) {
+      iconeSalvar = iconeMarcadorPreto
+    } else {
+      iconeSalvar = iconeMarcadorBranco
+    }
+
+    let componenteCompartilhar
+
+    if (this.state.compartilhar) {
+      componenteCompartilhar = <SecaoCompartilhar aoEnviar={this.aoEnviarCompartilhar}/>
     }
 
     let componenteComentario
@@ -73,7 +119,16 @@ class Post extends React.Component {
           onClickIcone={this.onClickCurtida}
           valorContador={this.state.numeroCurtidas}
         />
-
+        <IconeComContador
+          icone={iconeSalvar}
+          onClickIcone={this.onClickSalvar}
+          valorContador={this.state.numeroSalvados}
+        />
+        <IconeComContador
+        icone={iconeCompartilhar}
+        onClickIcone={this.onClickCompartilhar}
+        valorContador={this.state.numeroCompartilhamento}
+      />
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={this.onClickComentario}
@@ -81,6 +136,7 @@ class Post extends React.Component {
         />
       </div>
       {componenteComentario}
+      {componenteCompartilhar}
     </div>
   }
 }
--- semana5/insta4 2/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/insta4 2/src/components/SecaoComentario/SecaoComentario.js
@@ -7,7 +7,6 @@ export class SecaoComentario extends Component {
 	}
 
 	onChangeComentario() {
-
 	}
 
 	render() {
