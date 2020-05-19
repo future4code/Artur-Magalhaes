f5acb8e76d3a15459fb95324fc16fa5a324d134c
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-05-19 14:00:51 -0300

    pasta semana 6

---
+++ semana6/LabenuForms/labform/build/precache-manifest.07403b6fa352f7ffe82854aafd979343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed309a0e51af2d5522eb201273ddbc92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c53ad33a5b7c7ce05b67",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6401aeff1431100dda6",
+    "url": "/static/js/2.725f8c27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.725f8c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53ad33a5b7c7ce05b67",
+    "url": "/static/js/main.aeac2b59.chunk.js"
+  },
+  {
+    "revision": "846aca4e58737ca7a4ec",
+    "url": "/static/js/runtime-main.67aaad46.js"
+  }
+]);
