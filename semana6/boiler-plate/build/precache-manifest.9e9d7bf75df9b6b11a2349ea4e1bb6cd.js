6de1809383ba0c2785934fc4adf2f0d6def71910
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-05-19 16:44:18 -0300

    build

---
+++ semana6/boiler-plate/build/precache-manifest.9e9d7bf75df9b6b11a2349ea4e1bb6cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba330d092467ea624891c946646daa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20ad911ab08b0c81ee38",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "cf222051f0498bfb0faf",
+    "url": "/static/js/2.38e5fef9.chunk.js"
+  },
+  {
+    "revision": "20ad911ab08b0c81ee38",
+    "url": "/static/js/main.a49f84d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
