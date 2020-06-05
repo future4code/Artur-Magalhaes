6c5b09222c9b6adb35d8b1c4a3b576d92fa81cd3
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-06-05 13:00:47 -0300

    Implementação para adicionar playlist

--- semana8/labefy_project/labefy/src/App.js
+++ semana8/labefy_project/labefy/src/App.js
@@ -1,23 +1,60 @@
 import React from 'react';
+import styled from 'styled-components'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axiosConfig = {
+    headers: {
+      Authorization: 'artur-magalhaes-mello'
+    }
+}
+
+class App extends React.Component {
+  state = {
+    playlists: [],
+    name: '',
+  }
+
+  
+
+  addPlaylist = () => {
+
+    const data = {
+      name: this.state.name
+    }
+
+    axios.post('https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists', 
+      data, 
+    axiosConfig )
+    .then(response => {
+      this.setState({
+        name: ''
+      })
+      console.log(response) })
+    .catch(error => {
+      console.log(error) })   
+  }
+
+  //onChanges
+  onChangeAddPlaylist = (event) => {
+    this.setState({
+      name: event.target.value,
+    })
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+        <input placeholder='Criar Playlist' 
+          onChange={this.onChangeAddPlaylist}
+          value={this.state.name}
+        />
+        <button onClick={this.addPlaylist}>Adicionar Playlist</button>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
