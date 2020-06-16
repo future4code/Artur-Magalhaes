c0ad089b067224d36b1de977fcb3a8f8b19cc3d8
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-06-16 11:19:12 -0300

    Component Functionality Match

--- semana9/labematch/labematch/src/pages/Match/index.js
+++ semana9/labematch/labematch/src/pages/Match/index.js
@@ -7,7 +7,7 @@ import * as S from './style'
 export default function Match() {
   const [person, setPerson] = useState({});
 
-  useEffect(() => {
+  const getApi = () => {
     api.get('person')
       .then(response => {
         console.log(response.data.profile)
@@ -16,9 +16,45 @@ export default function Match() {
       .catch(err => {
         console.log(err)
       })
+  }
+
+  useEffect(() => {
+    getApi()
   },[])
 
+  const choosePerson = (data) => {
+    getApi()
+    api.post('choose-person', data)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const onClickMatchTrue = (id) => {
+    const data = {
+      "id": id,
+      "choice": true,
+    }
+    console.log(data)
+    choosePerson(data)
+  }
+
+  const onClickMatchFalse = (id) => {
+    const data = {
+      "id": id,
+      "choice": false,
+    }
+    choosePerson(data)
+  }
+
   return(
-    <S.ImagePerfil src={person.photo}/>
+    <>
+      <S.ImagePerfil src={person.photo}/>
+      <button onClick={() => onClickMatchFalse(person.id)}>X</button>
+      <button onClick={() => onClickMatchTrue(person.id)}>S2</button>
+    </>
     )
 }
