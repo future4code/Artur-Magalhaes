2c2fed3d1036765f50712a5df1ab227224d8d4ac
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-06-23 14:07:25 -0300

    CONFIG: Routes

---
+++ semana10/labetrip/src/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Apply from './pages/ApplyTripPage';
+import CreateTrip from './pages/CreateTripPage';
+import DetailTrip from './pages/DetailTripPage';
+import Home from './pages/HomePage';
+import ListTrip from './pages/ListTripPage';
+import Login from './pages/LoginPage';
+
+export default function Routes() {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/apply" exact component={Apply} />
+        <Route path="/createtrip" exact component={CreateTrip} />
+        <Route path="/detailtrip" exact component={DetailTrip} />
+        <Route path="/listtrip" exact component={ListTrip} />
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </BrowserRouter>
+    );
+}
