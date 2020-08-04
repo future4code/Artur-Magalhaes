08c04ca68db7d69e2946eb8314bfea3771eb78a8
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-04 12:14:01 -0300

    Exercicio 2

--- semana15/aula2/src/index.ts
+++ semana15/aula2/src/index.ts
@@ -1,4 +1,5 @@
 import { User } from './User'
+import { Customer } from './Customer'
 
 const user = new User("12345", "artur@gmail.com", "Artur", "123456")
 
@@ -6,3 +7,8 @@ console.log(user)
 //1.a) Não seria possível imprimir password por que está com modificador private e não possui um get públco
 //1.b) Imprime uma vez o console.log
 
+const customer = new Customer("23456", "astrodev@gmail.com", "Astrodev", "234567", "123456789101")
+
+console.log(customer)
+//2.a) Imprime uma vez o console.log
+//2.b) Foi impressa uma vez por que está presente no construtor da classe pai
