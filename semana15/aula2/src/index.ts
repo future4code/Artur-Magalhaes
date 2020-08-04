79dcf5a6b6bad22fecefa84cb6eb0fd53e70dbc5
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-04 12:16:45 -0300

    Exercicio 3

--- semana15/aula2/src/index.ts
+++ semana15/aula2/src/index.ts
@@ -12,3 +12,11 @@ const customer = new Customer("23456", "astrodev@gmail.com", "Astrodev", "234567
 console.log(customer)
 //2.a) Imprime uma vez o console.log
 //2.b) Foi impressa uma vez por que está presente no construtor da classe pai
+
+console.log(
+    customer.getCreditCard(),
+    customer.getEmail(),
+    customer.getId(),
+    customer.getName(),
+    customer.purchaseTotal)
+//3.a) Não por que não tem nenhum método público para mostrar esse atributo
