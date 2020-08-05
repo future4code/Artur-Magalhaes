a9602878751303083f10c7f7d3eac849e2974bad
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-05 12:06:39 -0300

    Exercicio 2

--- semana15/aula3/src/index.ts
+++ semana15/aula3/src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from './Client';
+import { Place } from './Place';
 
 const client: Client = {
     name: "Artur",
@@ -19,3 +20,8 @@ console.log(
 
 //1.a) Todas as informações foram impressas
 
+const place = new Place("60000000")
+console.log(place)
+
+//2.a) Não é possível criar instancia de uma classe abstrata
+//2.b) Deve criar outra classe que estenda a classe abstrata.
