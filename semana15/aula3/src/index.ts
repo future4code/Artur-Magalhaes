04eb4850a02d1067d3cf39eeba0ce764fb0d9e08
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-05 13:56:11 -0300

    Exercicio 5

--- semana15/aula3/src/index.ts
+++ semana15/aula3/src/index.ts
@@ -3,6 +3,8 @@ import { Place } from './Place';
 import { Residence } from './Residence';
 import { Commerce } from './Commerce';
 import { Industry } from './Industry';
+import { ResidentialClient } from './ResidentialClient';
+import { CommercialClient } from './CommercialCIient';
 
 const client: Client = {
     name: "Artur",
@@ -44,3 +46,15 @@ console.log(
 
 //4.a) Deve-se implementar os atributos e métodos da interface Client (name, registrationNumber, consumedEnergy) e da superclasse Residence (cep e residentsQuantity)
 //     Tem que implementar também o método para pegar o item privado (cpf)
+
+const residentialClient = new ResidentialClient("11111111111", "Astrodev", 1, 100, 2, "60000003")
+const commercialClient = new CommercialClient("111111111000192", "CearaSC", 2, 1000, 3, "60100200")
+console.log(
+    residentialClient,
+    residentialClient.calculateBill(),
+    commercialClient,
+    commercialClient.calculateBill()
+    )
+//5.a) Implementa a interface Client com seus atributos e métodos e estende a classe abstrata Place que possui o atributo cep.
+//5.b) Atributo cpf em ResidentialClient e cnpj em CommercialClient, e a implementação do método da interface
+
