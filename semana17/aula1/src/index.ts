300c34ea8ff0b858ad2336b7c042633d6c12cfac
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-08-17 16:33:13 -0300

    semana17-aula1

--- semana17/aula1/src/index.ts
+++ semana17/aula1/src/index.ts
@@ -1,7 +1,10 @@
 import knex from 'knex';
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { AddressInfo } from 'net';
+import { UserDataBase } from './data/UserDataBase';
+import { IdGenerator } from './service/IdGenerator';
+import { Authenticator } from './service/Authenticator';
 
 dotenv.config();
 
@@ -28,3 +31,82 @@ const connection = knex({
     }
 });
 
+//const user = new CreateUser()
+//user.createUsers("002", "Artur2", "arturmm2", "123456","artur2@gmail.com");
+
+app.post('/signup', async (req: Request, res: Response) => {
+    try {
+        if(!req.body.email || req.body.email.indexof("@") === -1){
+            throw new Error("Invalid email");
+        }
+
+        if(!req.body.password || req.body.password.length < 6){
+            throw new Error("Invalid password");
+        }
+
+        const data = {
+            'name': req.body.name,
+            'nickname': req.body.nickname,
+            'email': req.body.email,
+            'password': req.body.password,
+        }
+        const idGenerator = new IdGenerator();
+        const id = idGenerator.generate();
+
+        const user = new UserDataBase()
+        await user.createUsers(id, data.name, data.nickname, data.password, data.email);
+
+        const authenticator = new Authenticator();
+        const token = authenticator.generateToken({
+            id,
+        });
+
+        res.status(200).send({
+            token
+        });
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+        })
+    }
+})
+
+app.get('/signin', async (req: Request, res: Response) => {
+    try {
+        if (!req.body.email || req.body.email.indexOf('@') === -1){
+            throw new Error ("Invalid Email")
+        }
+        const data = {
+            'email': req.body.email,
+            'password': req.body.password,
+        }
+        const user = new UserDataBase();
+        const result = await user.signIn(data.email, data.password)
+        
+        const authenticator = new Authenticator()
+        const token = authenticator.generateToken(result[0].id)
+
+        res.status(200).send({
+            "token": token})
+    } catch (error) {
+        res.status(400).send({
+            message: error.message
+        })
+    }
+})
+
+app.get('/user/profile', async (req: Request, res: Response) => {
+    try {
+        const user = new UserDataBase();
+        const authenticator = new Authenticator();
+
+        const auth = authenticator.getData(req.headers.authorization as string);
+        console.log(auth)
+        const result = await user.getUsers(auth.id);
+        res.status(200).send(result)
+    } catch (error) {
+        res.status(401).send({
+            message: error.message
+        })
+    }
+})
